2b5b30436d176164ac8ad35572787c50a7837dd5
prashantalhat1406 <prashant.alhat@gmail.com>
2020-07-05 15:36:56 +0530

    firebase Auth

--- app/src/main/java/com/example/bigmart/adapterProduct.java
+++ app/src/main/java/com/example/bigmart/adapterProduct.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -90,60 +91,61 @@ public class adapterProduct extends ArrayAdapter<Product> {
         database = FirebaseDatabase.getInstance("https://bigmart-sinprl.firebaseio.com/");
 
 
-        product = products.get(position);
-
-        ImageView productImage = (ImageView) convertView.findViewById(R.id.img_product_image);
-        int id_ = context.getResources().getIdentifier(product.ID.toLowerCase(), "drawable", context.getPackageName());
-        int noimage = context.getResources().getIdentifier("imagenotavailable", "drawable", context.getPackageName());
-        int outofstock = context.getResources().getIdentifier("outofstock", "drawable", context.getPackageName());
-
-        if (id_ == 0)
-            productImage.setImageResource(noimage);
-        else
-            productImage.setImageResource(id_);
-
-        if (product.Qty < product.MinStock)
-        {
-            atc.setEnabled(false);
-            atc.setBackground(context.getDrawable(R.drawable.status_complete_disable));
-            productImage.setImageResource(outofstock);
-        }else {
-            atc.setEnabled(true);
-            atc.setBackground(context.getDrawable(R.drawable.roundbutton_green));
-        }
-
-        TextView Name = (TextView)convertView.findViewById(R.id.txt_product_product_Name);
-        Name.setText(""+product.getName());
-        TextView productQuantity = (TextView)convertView.findViewById(R.id.txt_product_product_QTY);
-        productQuantity.setText(""+product.getName2().toLowerCase());
-        TextView mrp = (TextView)convertView.findViewById(R.id.txt_product_product_MRP);
-        mrp.setText(context.getResources().getString(R.string.Rupee) + " "+ formater.format( product.MRP));
-        mrp.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
-        TextView discount = (TextView)convertView.findViewById(R.id.txt_product_product_BMA);
-        discount.setText(context.getResources().getString(R.string.Rupee) +" " + formater.format(product.MRP -  product.Discount.doubleValue()));
-        final TextView savedamount = (TextView)convertView.findViewById(R.id.txt_product_product_Save);
-        final TextView bmaamount = (TextView)convertView.findViewById(R.id.txt_product_product_BMA);
+        try {
 
 
-        if(product.QtyNos == 0) {
-            savedamount.setText(context.getResources().getString(R.string.Rupee) + " " +  formater.format (product.Discount * 1));
-            bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " +  formater.format((product.MRP - product.Discount )));
+            product = products.get(position);
 
-        }
-        else {
-            savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(product.Discount * product.QtyNos));
-            bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format( ((product.MRP - product.Discount ) * product.QtyNos)) );
+            ImageView productImage = (ImageView) convertView.findViewById(R.id.img_product_image);
+            int id_ = context.getResources().getIdentifier(product.ID.toLowerCase(), "drawable", context.getPackageName());
+            int noimage = context.getResources().getIdentifier("imagenotavailable", "drawable", context.getPackageName());
+            int outofstock = context.getResources().getIdentifier("outofstock", "drawable", context.getPackageName());
 
-        }
+            if (id_ == 0)
+                productImage.setImageResource(noimage);
+            else
+                productImage.setImageResource(id_);
 
-        final TextView itemNos = (TextView) convertView.findViewById(R.id.txt_product_item_number);
-        itemNos.setText(""+product.getQtyNos());
+            if (product.Qty < product.MinStock) {
+                atc.setEnabled(false);
+                atc.setBackground(context.getDrawable(R.drawable.status_complete_disable));
+                productImage.setImageResource(outofstock);
+            } else {
+                atc.setEnabled(true);
+                atc.setBackground(context.getDrawable(R.drawable.roundbutton_green));
+            }
 
-        Button plus = (Button) convertView.findViewById(R.id.but_product_item_plus);
-        plus.setTag(position);
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            TextView Name = (TextView) convertView.findViewById(R.id.txt_product_product_Name);
+            Name.setText("" + product.getName());
+            TextView productQuantity = (TextView) convertView.findViewById(R.id.txt_product_product_QTY);
+            productQuantity.setText("" + product.getName2().toLowerCase());
+            TextView mrp = (TextView) convertView.findViewById(R.id.txt_product_product_MRP);
+            mrp.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(product.MRP));
+            mrp.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+            TextView discount = (TextView) convertView.findViewById(R.id.txt_product_product_BMA);
+            discount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(product.MRP - product.Discount.doubleValue()));
+            final TextView savedamount = (TextView) convertView.findViewById(R.id.txt_product_product_Save);
+            final TextView bmaamount = (TextView) convertView.findViewById(R.id.txt_product_product_BMA);
+
+
+            if (product.QtyNos == 0) {
+                savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(product.Discount * 1));
+                bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format((product.MRP - product.Discount)));
+
+            } else {
+                savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(product.Discount * product.QtyNos));
+                bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount) * product.QtyNos)));
+
+            }
+
+            final TextView itemNos = (TextView) convertView.findViewById(R.id.txt_product_item_number);
+            itemNos.setText("" + product.getQtyNos());
+
+            Button plus = (Button) convertView.findViewById(R.id.but_product_item_plus);
+            plus.setTag(position);
+            plus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
                 /*int t = getTempOrderProductCount();
                 if (getTempOrderProductCount() < 90){*/
                     Intent bintIntent = new Intent("message_subject_intent");
@@ -151,9 +153,8 @@ public class adapterProduct extends ArrayAdapter<Product> {
                     LocalBroadcastManager.getInstance(context).sendBroadcast(bintIntent);
 
 
-
                     product = products.get((Integer) v.getTag());
-                    if(product.QtyNos < 5) {
+                    if (product.QtyNos < 5) {
                         product.setQtyNos(product.getQtyNos() + 1);
                         DatabaseReference databaseReference = database.getReference("Users/" + userID + "/TempOrder");
                         databaseReference.child("" + product.ID).setValue(product);
@@ -161,7 +162,7 @@ public class adapterProduct extends ArrayAdapter<Product> {
                         incrementor.setVisibility(View.VISIBLE);
                         savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format((product.Discount * product.QtyNos)));
                         bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount) * product.QtyNos)));
-                    }else{
+                    } else {
 
                         AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
                         builder1.setMessage("Max Limit Reached");
@@ -184,73 +185,71 @@ public class adapterProduct extends ArrayAdapter<Product> {
                         alert11.show();
 
                     }
-                //}
+                    //}
 
-            }
-        });
-
-        Button minus = (Button) convertView.findViewById(R.id.but_product_item_minus);
-        minus.setTag(position);
-        minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent bintIntent = new Intent("message_subject_intent");
-                bintIntent.putExtra("position", position);
-                LocalBroadcastManager.getInstance(context).sendBroadcast(bintIntent);
-
-                //final FirebaseDatabase database = FirebaseDatabase.getInstance("https://bigmart-sinprl.firebaseio.com/");
-                product = products.get((Integer) v.getTag());
-                product.setQtyNos(product.getQtyNos() - 1);
-                DatabaseReference databaseReference = database.getReference("Users/"+userID+"/TempOrder");
-                databaseReference.child(""+product.ID).setValue(product);
-                itemNos.setText(""+product.QtyNos);
-                if (product.QtyNos == 0)
-                {
-                    atc.setVisibility(View.VISIBLE);
-                    incrementor.setVisibility(View.GONE);
-                    databaseReference.child(""+product.ID).removeValue();
-                    savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format( ( product.Discount)));
-                    bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount ))));
                 }
-                else
-                {
+            });
+
+            Button minus = (Button) convertView.findViewById(R.id.but_product_item_minus);
+            minus.setTag(position);
+            minus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent bintIntent = new Intent("message_subject_intent");
+                    bintIntent.putExtra("position", position);
+                    LocalBroadcastManager.getInstance(context).sendBroadcast(bintIntent);
+
+                    //final FirebaseDatabase database = FirebaseDatabase.getInstance("https://bigmart-sinprl.firebaseio.com/");
+                    product = products.get((Integer) v.getTag());
+                    product.setQtyNos(product.getQtyNos() - 1);
+                    DatabaseReference databaseReference = database.getReference("Users/" + userID + "/TempOrder");
+                    databaseReference.child("" + product.ID).setValue(product);
+                    itemNos.setText("" + product.QtyNos);
+                    if (product.QtyNos == 0) {
+                        atc.setVisibility(View.VISIBLE);
+                        incrementor.setVisibility(View.GONE);
+                        databaseReference.child("" + product.ID).removeValue();
+                        savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format((product.Discount)));
+                        bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount))));
+                    } else {
+                        incrementor.setVisibility(View.VISIBLE);
+                        savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format((product.Discount * product.QtyNos)));
+                        bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount) * product.QtyNos)));
+                    }
+                }
+            });
+
+
+            if (product.getQtyNos() > 0) {
+                atc.setVisibility(View.GONE);
+                incrementor.setVisibility(View.VISIBLE);
+            } else {
+                atc.setVisibility(View.VISIBLE);
+                incrementor.setVisibility(View.GONE);
+            }
+
+
+            atc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //final FirebaseDatabase database = FirebaseDatabase.getInstance("https://bigmart-sinprl.firebaseio.com/");
+                    product = products.get((Integer) v.getTag());
+                    product.setQtyNos(1);
+                    DatabaseReference databaseReference = database.getReference("Users/" + userID + "/TempOrder");
+                    databaseReference.child("" + product.ID).setValue(product);
+                    itemNos.setText("" + product.QtyNos);
+                    v.setVisibility(View.GONE);
                     incrementor.setVisibility(View.VISIBLE);
                     savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format((product.Discount * product.QtyNos)));
-                    bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount ) * product.QtyNos)));
+                    bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount))));
                 }
-            }
-        });
+            });
 
 
-        if(product.getQtyNos() > 0) {
-            atc.setVisibility(View.GONE);
-            incrementor.setVisibility(View.VISIBLE);
+
+        }catch (Exception e){
+            Log.d("Ts",e.getMessage());
         }
-        else {
-            atc.setVisibility(View.VISIBLE);
-            incrementor.setVisibility(View.GONE);
-        }
-
-
-
-
-        atc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //final FirebaseDatabase database = FirebaseDatabase.getInstance("https://bigmart-sinprl.firebaseio.com/");
-                product = products.get((Integer) v.getTag());
-                product.setQtyNos(1);
-                DatabaseReference databaseReference = database.getReference("Users/"+userID+"/TempOrder");
-                databaseReference.child(""+product.ID).setValue(product);
-                itemNos.setText(""+product.QtyNos);
-                v.setVisibility(View.GONE);
-                incrementor.setVisibility(View.VISIBLE);
-                savedamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format((product.Discount * product.QtyNos)));
-                bmaamount.setText(context.getResources().getString(R.string.Rupee) + " " + formater.format(((product.MRP - product.Discount ))));
-            }
-        });
-
-
         return convertView;
     }
 }
